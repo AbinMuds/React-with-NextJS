8e9675faf4f2a58f0d8b8359f8b7a57bd2a07355
AbinMuds <abinashmudbhari@gmail.com>
2022-04-10 15:22:04 -0600

    adds new-meetup for request POST in api with mongodb

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetUpPage() {
-    function addMeetupHandler(enteredMeetupData) {
-        console.log(enteredMeetupData)
-    }
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+    router.push("/");
+  }
   return (
-        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
-  )
+    <React.Fragment>
+      <Head>
+        <title>Add a new meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities"
+        ></meta>
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </React.Fragment>
+  );
 }
 
-export default NewMeetUpPage
+export default NewMeetUpPage;
